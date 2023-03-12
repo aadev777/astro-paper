e232f08fe802c9165380b668f981db1d229d158c
Ameer Abbas <ameer00@gmail.com>
2023-03-12 05:11:04 +0000

    init

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,8 +7,8 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://aadev777.github.io/",
-  base: "astro-paper",
+  site: "https://aadev777.github.io",
+  base: "/astro-paper",
   integrations: [
     tailwind({
       config: {
